751b01dd40a574e3632f1f1174c8f9fd80ed2c34
hajos8 <rabb.andras@gmail.com>
2025-12-15 08:31:28 +0100

    Create form with 2 columns

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
-import pandas as pd
- 
-st.write("""
-# My first app
-Hello *world!*
-""")
+
+with st.form(key='my_form'):
+    col1, col2 = st.columns(2)
+    with col1:
+        name_label = st.subheader('Name')
+    with col2:
+        name_input = st.text_input(label='Enter your name')
+
+    submit = st.form_submit_button(label='Submit')
